7b2e0565b31a690f1103456ae7d92927f531965b
Denisios271 <guzden.272@gmail.com>
2022-04-21 15:55:36 +0200

    Rename industrial portal to the Fleet

--- FE/src/code/helpers/routesHelpers.js
+++ FE/src/code/helpers/routesHelpers.js
@@ -2,6 +2,8 @@ export function getStation(station) {
   if (station) {
     return {
       station: { id: station.id },
+      latitude: station.latitude,
+      longitude: station.longitude,
     };
   }
   return undefined;
