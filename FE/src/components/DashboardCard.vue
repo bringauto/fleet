1410bfdfa77a7707df3828ae2adcfd233b704db8
JakubNajvarek <jakub.najvarek@gmail.com>
2023-04-17 10:14:30 +0200

    duplicate removed

--- FE/src/components/DashboardCard.vue
+++ FE/src/components/DashboardCard.vue
@@ -13,7 +13,7 @@
       <!--<p class="text-center text-h6 mb-0">{{ car.name }}</p>-->
       <div class="d-flex justify-center align-center text-caption mb-1">
         <span v-if="car.fuel" class="mr-2">
-          <v-icon>{{ getCarBatteryIcon(car.fuel.toFixed(4) || car.fuel.toFixed(4)) }}</v-icon>
+          <v-icon>{{ getCarBatteryIcon(car.fuel.toFixed(4)) }}</v-icon>
           {{ car.fuel.toFixed(4) * 100 }}%
         </span>
         <span>{{ getLastUpdate(car) }}</span>
